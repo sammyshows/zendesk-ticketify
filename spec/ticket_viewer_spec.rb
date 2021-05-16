78bb76db28a4ecde36d1313c545bec33b1d417f4
Sam McCarthy <samrmccarthy6@gmail.com>
2021-05-16 14:09:59 +1000

    Created first test to verify the ticket count

--- spec/ticket_viewer_spec.rb
+++ spec/ticket_viewer_spec.rb
@@ -1,1 +1,8 @@
 require 'ticket_viewer'
+
+describe TicketViewer do
+  it 'should return all 100 tickets' do
+    ticket_viewer = TicketViewer.new(ENV['ZENDESK_API_KEY'])
+    expect(ticket_viewer.index).to eq(100)
+  end
+end
