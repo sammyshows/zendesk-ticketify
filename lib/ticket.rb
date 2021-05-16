32e168a64f827c23c4ede20a0c2a0d36365ad200
Sam McCarthy <samrmccarthy6@gmail.com>
2021-05-16 19:39:49 +1000

    Made ticket repository for storing tickets

--- lib/ticket.rb
+++ lib/ticket.rb
@@ -2,6 +2,7 @@ class Ticket
   attr_reader :id, :status, :priority, :subject, :description, :due_at, :assignee_id
 
   def initialize(attributes = {})
+    @app_id = attributes[:app_id]
     @id = attributes[:id]
     @status = attributes[:meal]
     @priority = attributes[:priority]
---
+++ lib/ticket_repository.rb
@@ -0,0 +1,28 @@
+class TicketRepository
+
+  def initialize(ticket_data)
+    @ticket_data = ticket_data
+    @tickets = []
+    @app_ticket_id = 1
+    load_tickets
+  end
+
+  private
+
+  def load_tickets
+    ticket_data.each do |ticket|
+      ticket_hash = {}
+      ticket_hash[:app_ticket_id] = @app_ticket_id
+      ticket_hash[:id] = ticket["id"]
+      ticket_hash[:status] = ticket["status"]
+      ticket_hash[:priority] = ticket["priority"]
+      ticket_hash[:subject] = ticket["subject"]
+      ticket_hash[:description] = ticket["description"]
+      ticket_hash[:due_at] = ticket["due_at"]
+      ticket_hash[:assignee] = ticket["assignee"]
+
+      @tickets << Ticket.new(ticket_hash)
+      @app_ticket_id += 1
+    end
+  end
+end
